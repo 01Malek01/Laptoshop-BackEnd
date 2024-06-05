03f16509be660b73da41a9e1710b5883e8b84a36
Malek Mostafa <96378187+01Malek01@users.noreply.github.com>
2024-06-05 17:46:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ cloudinary.config({
 
 // CORS configuration
 const allowedOrigins = [
-  'https://laptoshop-front-5saul04hf-maleks-projects-4df77c24.vercel.app',
+  'https://laptoshop-front-end.vercel.app/',
   'http://localhost:3000',
 ];
 
